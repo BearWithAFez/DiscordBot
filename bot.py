1fe757c608d360b851e911b6d0b8246db446e8f9
Dwight VdV <dfr34kz@gmail.com>
2020-01-10 14:52:01 +0100

    simplify

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -7,10 +8,5 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     print("We're soaring!!")
-
-# Ping command
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'{ctx.message.author.mention} Pong! ({round(client.latency * 1000)}ms)')
 
 client.run('NjYyNTUzMDQzMzg0MTM5Nzg2.Xg7osQ.kXo4my7NoMSnF_3z5F3F8AtRI_k')
